e67c9d9962ea57a425c7670882d039e2e2398dd3
BrendanMcKendry <161656761+BrendanMcKendry@users.noreply.github.com>
2024-03-13 00:29:30 -0400

    Added billing and client side app

--- models/User.js
+++ models/User.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  googleId: String
+  googleId: String,
+  //track user credits, default is 0
+  credits: { type: Number, default: 0 }
 });
 
 mongoose.model('users', userSchema);
