e6fdd94118ebfbf5cbdc0e50dc40526ea9caa787
BrendanMcKendry <161656761+BrendanMcKendry@users.noreply.github.com>
2024-03-28 19:09:28 -0400

    email service implementation (WIP)

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,11 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-  app.get('/api/surveys/thanks', (req, res) => {
-    res.send('Thanks for voting!');
-  });
-
-  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -27,16 +23,6 @@ module.exports = (app) => {
 
     // Great place to send an email!
     const mailer = new Mailer(survey, surveyTemplate(survey));
-
-    try {
-      await mailer.send();
-      await survey.save();
-      req.user.credits -= 1;
-      const user = await req.user.save();
-
-      res.send(user);
-    } catch (err) {
-      res.status(422).send(err);
-    }
+    mailer.send();
   });
 };
